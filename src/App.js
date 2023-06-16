be7dc9559cb06e0a14044eaa5a42c1a55d9179da
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-16 15:27:43 +0530

    parameter

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ function App() {
   return (
     <>
       <h1>My App</h1>
+      <MessageDemo name="Vaibhav" email="1" />
+      <MessageDemo email="2" name="Vaibhavv" />
+    </>
+  );
+}
+
+function MessageDemo({ email, name }) {
+  return (
+    <>
+      <h1>{name}</h1>
+      <h1>{email}</h1>
     </>
   );
 }
